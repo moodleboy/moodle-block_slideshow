5d1a87e583a3a10495858453f60a27c899d6d546
moodleboy <paul@Pauls-MacBook-Pro-2.local>
2013-09-13 01:27:01 -0400

    Update version.php
    version.php updated to include new version number, maturity, and
    release.

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
  */
 
 
-$plugin->version = 2013083100; // YYYYMMDDHH (year, month, day, 24-hr time) 
+$plugin->version = 2013091200; // YYYYMMDDHH (year, month, day, 24-hr time) 
 $plugin->requires = 2013040500 ; // YYYYMMDDHH (This is the release version for Moodle 2.0)
-$plugin->maturity = MATURITY_ALPHA;
-$plugin->release = "0.2.0";
+$plugin->maturity = MATURITY_BETA;
+$plugin->release = "0.3.0";
